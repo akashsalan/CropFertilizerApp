3cb71179badf37326ad0738346f7d3b023be2cbe
Akash Salan <74914636+akashsalan@users.noreply.github.com>
2025-05-16 19:57:18 +0530

    app.py

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ with st.form("input_form"):
     P = st.number_input("Phosphorous (P)", min_value=0, max_value=200, value=42)
     K = st.number_input("Potassium (K)", min_value=0, max_value=200, value=43)
 
-    temperature = st.number_input("Temperature (°C)", min_value=10.0, max_value=45.0, value=22.0, step=0.01)
-    humidity = st.number_input("Humidity (%)", min_value=20.0, max_value=100.0, value=82.0, step=0.01)
-    ph = st.number_input("Soil pH", min_value=3.0, max_value=10.0, value=6.5, step=0.01)
+    temperature = st.number_input("Temperature (°C)", min_value=0.0, max_value=45.0, value=22.0, step=0.01)
+    humidity = st.number_input("Humidity (%)", min_value=0.0, max_value=100.0, value=82.0, step=0.01)
+    ph = st.number_input("Soil pH", min_value=0.0, max_value=10.0, value=6.5, step=0.01)
     rainfall = st.number_input("Rainfall (mm)", min_value=0.0, max_value=300.0, value=200.0, step=0.01)
     soil_type = st.selectbox("Soil Type", ["sandy", "loamy", "black", "red", "clay"], index=0)
 
